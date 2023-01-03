1b6c9c44b7d6736e720b0537104fe775923daa54
moasfar javed <moasfarjaved@gmail.com>
2023-01-03 10:00:42 +0500

    init

---
+++ frontend/src/components/it-dept/it-crud-emp.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ItSidebar from "./it-sidebar";
+
+function ItCRUD() {
+  return (
+    <div className="App">
+        <ItSidebar/>
+    </div>
+  );
+}
+
+export default ItCRUD;
---
+++ frontend/src/components/it-dept/it-dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ItSidebar from "./it-sidebar";
+
+function ItDashboard() {
+  return (
+    <div className="App">
+      <ItSidebar/>
+    </div>
+  );
+}
+
+export default ItDashboard;
