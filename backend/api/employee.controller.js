9dedc0dc233343f67da42dd6d25c8cff647aa04f
moasfar javed <moasfarjaved@gmail.com>
2023-01-10 02:43:19 +0500

    employee crud completed

--- backend/api/employee.controller.js
+++ backend/api/employee.controller.js
@@ -25,4 +25,78 @@ export default class EmployeeController {
         }
         res.json(response)
     }
+    
+    static async apiPostEmployees(req, res, next) {
+        try {
+            const name = req.body.name
+            const designation = req.body.designation
+            const email = req.body.email
+            const phone = req.body.phone
+            const attendance = req.body.attendance
+            const salary = req.body.salary
+
+            const employeeResponse = await EmployeeDAO.addEmployee(
+                name,
+                designation,
+                email,
+                phone,
+                attendance,
+                salary,
+            )
+            res.json({ Status: "Success"})
+        } catch (e) {
+            res.status(500).json({Error: e.message})
+        }
+    }
+
+    static async apiPutEmployees(req, res, next) {
+        try {
+            const name = req.body.name
+            const designation = req.body.designation
+            const email = req.body.email
+            const phone = req.body.phone
+            const attendance = req.body.attendance
+            const salary = req.body.salary
+            
+            const employeeResponse = await EmployeeDAO.updateEmployee(
+                req.body.id,
+                name,
+                designation,
+                email,
+                phone,
+                attendance,
+                salary,
+            )
+
+            var { error } = employeeResponse
+            if (error) {
+                res.status(400).json({error})
+            }
+
+            if (employeeResponse.modifiedCount === 0){
+                throw new Error(
+                    "Unable to update employee",
+                )
+            }
+
+            res.json({ Status: "Success" })
+        } catch (e) {
+            res.status(500).json({ Error: e.message })
+        }
+
+        
+    }
+
+    static async apiDeleteEmployees(req, res, next) {
+        try {
+            const id = req.query.id
+            console.log(id)
+            const employeeResponse = await EmployeeDAO.deleteEmployee(
+                id,
+            )
+            res.json({ Status: "success" })
+        } catch (e) {
+            res.status(500).json({ Error: e.message })
+        }
+    }
 }
--- backend/dao/employeesDAO.js
+++ backend/dao/employeesDAO.js
@@ -1,3 +1,7 @@
+import mongodb from "mongodb"
+import { useResolvedPath } from "react-router-dom"
+const ObjectId = mongodb.ObjectId
+
 let employees
 
 export default class EmployeeDAO {
@@ -46,6 +50,55 @@ export default class EmployeeDAO {
                 `Unable to convert cursor to array or problem counting documents, ${e}`
             )
             return { employeesList: [], totalNumEmployee: 0 }
+        }
+    }
+
+    static async addEmployee(name, designation, email, phone, attendance, salary) {
+        try {
+            const employeeDoc = {
+                name: name,
+                designation: designation,
+                email: email,
+                phone: phone,
+                attendance: attendance,
+                salary: salary,
+            }
+            return await employees.insertOne(employeeDoc)
+        } catch (e) {
+            console.error(`Unable to add employee: ${e}`)
+            return { Error: e }
+        }
+    } 
+
+    static async updateEmployee(id, name, designation, email, phone, attendance, salary) {
+        try {
+            const updateResponse = await employees.updateOne(
+                { _id: ObjectId(id) },
+                { $set: { 
+                    name: name,
+                    designation: designation,
+                    email: email,
+                    phone: phone,
+                    attendance: attendance,
+                    salary: salary
+                }},
+            )
+            return updateResponse
+        } catch (e) {
+            console.error(`Unable to update employee: ${e}`)
+            return { Error: e }
+        }
+    }
+
+    static async deleteEmployee(id){
+        try {
+            const deleteResponse = await employees.deleteOne({
+                _id: ObjectId(id)
+            })
+            return deleteResponse
+        } catch (e) {
+            console.error(`Unable to delete employee: ${e}`)
+            return { Error: e }
         }
     }
 }
