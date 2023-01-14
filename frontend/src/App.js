579bad77dd328fb36ef782231dd2da69045ba3d7
moasfar javed <moasfarjaved@gmail.com>
2023-01-14 14:06:09 +0500

    maps

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import './css/style.css';
 import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
 import "primereact/resources/primereact.min.css";                  //core css
 import "primeicons/primeicons.css"; 
+
 function App(props) {
 
   const onEmployee = () => {
