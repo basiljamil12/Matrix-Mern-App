f313a004b85215b010b2256770b970e75111579b
Basil <basiljamil1122@gmail.com>
2023-01-15 16:35:42 +0500

    Pushing for pull

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+
 import { React, useState, useEffect } from 'react';
 import { Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -12,6 +13,7 @@ import './css/style.css';
 import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
 import "primereact/resources/primereact.min.css";                  //core css
 import "primeicons/primeicons.css";
+import SupSidebar from './container/supervisor/sup_sidebar';
 
 let design;
 
@@ -40,6 +42,13 @@ function App(props) {
   const onLogistics = () => {
     window.location = "#/logistics";
   };
+  const onTask = () => {
+    window.location = "#/tasks";
+  };
+  const onBonus = () => {
+    window.location = "#/bonuslist";
+  };
+
 
   window.onbeforeunload = function () {
     localStorage.removeItem("data");
@@ -49,50 +58,20 @@ function App(props) {
 
     <div className="wrapper d-flex align-items-stretch">
 
-      <nav    id="sidebar">
-        <div  className="p-4 pt-5 bg-dark">
-          <h3 style={{ textAlign: 'center', color: "white" }}>EMPLOYEE</h3>
-   
-          <hr style={ {backgroundColor: "white"} }/>
-          <ul  className="nav nav-pills flex-column mb-auto">
-        
-            <button type="button" className="btn btn-dark w-100" onClick={() => { onEmployee() }}>Employee</button><br></br>
-            <button type="button" className="btn btn-dark" >Tasks</button><br></br>
-            <button type="button" className="btn btn-dark" onClick={() => { onLogistics() }}>Logistics</button><br></br>
-            <button type="button" className="btn btn-dark">Machine Information</button><br></br>
-            <button type="button" className="btn btn-dark">Refinery/Purity Status</button><br></br>
-            <hr style={ {backgroundColor: "white"} }/>
-            <button type="button" className="btn btn-dark">Attendance</button><br></br>
-            <button type="button" className="btn btn-dark">Salary</button><br></br>
-            <button type="button" className="btn btn-dark">Bonuses</button><br></br>
-
-          </ul>
-         
-          <hr style={ {backgroundColor: "white"} }/>
-          <h3 style={{textAlign: 'center', color: "white"}}>Admin</h3><br />
-          <button type="button" className="btn btn-outline-danger btn-block" onClick={() => { onLogOut() }} >Sign out</button>
-          
-          
-          <div className="footer">
-
-          </div>
-
-        </div>
-      </nav>
+      
       {
         data.map((item, i) => (
           design = item.designation,
       <div className='d-flex'>
 
       {
-        (design === "admin") ? <ItSidebar /> : <EmpSidebar />
+        (design === "admin") ? <ItSidebar /> : ((design === "supervisor") ? <SupSidebar/> : <EmpSidebar />)
       }
       </div>
       
     
           
       ))}
-
       <div id="content" className="p-4 p-md-5">
         <Route path="/App" exact component={Dashboard} />
         <Routes />
--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -7,16 +7,14 @@ import detailList from '../container/emp/show_detail';
 import AddEmp from '../container/emp/add_emp';
 import EditEmp from '../container/emp/edit_emp';
 import AddLogs from '../container/logs/add_logs';
-import EditLogistics from '../container/logs/edit_logs';
+import EditLogistics from '../container/logs/edit_logs.js';
 import TaskList from '../container/task/tasks.js';
 import AddTask from '../container/task/add_task.js';
 import ViewTask from '../container/task/view_task.js';
 import BonusList from '../container/bonuses/bonuslist.js';
 import AddBonus from '../container/bonuses/addbonus.js';
-import ViewBonus from '../container/bonuses/viewbonus.js';
-import Dashboard from '../container/Dashboard';
-import loggedTasks from '../container/task/loggedTasks';
-import MachineInfo from '../container/machine/machineinfo';
+import Dashboard from '../container/Dashboard.js';
+import loggedTasks from '../container/task/loggedTasks.js';
 
 export const Routes = () => (
    
@@ -34,8 +32,6 @@ export const Routes = () => (
             <Route path="/view_task" component={ViewTask}/>
             <Route path="/bonuslist" component={BonusList}/>
             <Route path="/addbonus" component={AddBonus}/>
-            <Route path="/viewbonus" component={ViewBonus}/>
-            <Route path="/machineinfo" component={MachineInfo}/>
             <Route path="/empTasks" component={loggedTasks}/>
             <Route path="/dashboard" component={Dashboard}/>
         </div>
